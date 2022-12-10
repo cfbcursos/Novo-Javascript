97f5bd8723062594175e6bb18cc2a46d1268cd84
CFB Cursos <canalfessorbruno@gmail.com>
2022-12-10 17:13:58 -0300

    aulas JS 10_12_22

--- aula82-86.js
+++ aula82-86.js
@@ -7,33 +7,33 @@ const btn_remover=document.getElementById("btn_remover")
 let larguraPalco=palco.offsetWidth
 let alturaPalco=palco.offsetHeight
 let bolas=[]
-let numBolas=0
+let numBola=0
 
 class Bola{
-    constructor(arrayPai,palco){
-        this.tam=Math.floor(Math.random()*10)+10
-        this.arrayPai=arrayPai
-        this.id=Date.now()+"_"+Math.floor(Math.random()*100000000)
-        this.palco=palco
-        this.px=Math.floor(Math.random()*(larguraPalco-this.tam))
-        this.py=Math.floor(Math.random()*(alturaPalco-this.tam))
-        // this.vel=Math.floor(Math.random()*2)+0.5
-        this.velx=Math.floor(Math.random()*2)+0.5
-        this.vely=Math.floor(Math.random()*2)+0.5
-        this.dirx=Math.floor(Math.random()*10)>5?-1:1
-        this.diry=Math.floor(Math.random()*10)>5?-1:1
+    constructor(arrayBolas,palco){
+        this.tam=Math.floor(Math.random()*15)+10
         this.r=Math.floor(Math.random()*255)
         this.g=Math.floor(Math.random()*255)
         this.b=Math.floor(Math.random()*255)
+        this.px=Math.floor(Math.random()*(larguraPalco-this.tam))
+        this.py=Math.floor(Math.random()*(alturaPalco-this.tam))
+        this.velx=Math.floor(Math.random()*2)+0.5
+        this.vely=Math.floor(Math.random()*2)+0.5
+        this.dirx=(Math.random()*10)>5?1:-1
+        this.diry=(Math.random()*10)>5?1:-1
+        this.palco=palco
+        this.arrayBolas=arrayBolas
+        this.id=Date.now()+"_"+Math.floor(Math.random()*100000000000000)
         this.desenhar()
         this.controle=setInterval(this.controlar,10)
         this.eu=document.getElementById(this.id)
-        numBolas++
-        num_objetos.innerHTML=numBolas
+        numBola++
+        num_objetos.innerHTML=numBola
     }
     minhaPos=()=>{
-        return this.arrayPai.indexOf(this)
+        return this.arrayBolas.indexOf(this)
     }
+
     remover=()=>{
         clearInterval(this.controle)
         bolas=bolas.filter((b)=>{
@@ -42,17 +42,19 @@ class Bola{
             }
         })
         this.eu.remove()
-        numBolas--
-        num_objetos.innerHTML=numBolas
+        numBola--
+        num_objetos.innerHTML=numBola
     }
+
     desenhar=()=>{
         const div=document.createElement("div")
         div.setAttribute("id",this.id)
         div.setAttribute("class","bola")
-        div.setAttribute("style","left:"+this.px+"px;top:"+this.py+"px;width:"+this.tam+"px;height:"+this.tam+"px;background-color:rgb("+this.r+","+this.g+","+this.b+")")
+        div.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
         this.palco.appendChild(div)
     }
-    colisao_bordas=()=>{
+
+    controle_bordas=()=>{
         if(this.px+this.tam >= larguraPalco){
             this.dirx=-1
         }else if(this.px <= 0){
@@ -64,12 +66,12 @@ class Bola{
             this.diry=1
         }
     }
+
     controlar=()=>{
-        this.eu=document.getElementById(this.id)
-        this.colisao_bordas()
+        this.controle_bordas()
         this.px+=this.dirx*this.velx
         this.py+=this.diry*this.vely
-        this.eu.setAttribute("style","left:"+this.px+"px;top:"+this.py+"px;width:"+this.tam+"px;height:"+this.tam+"px;background-color:rgb("+this.r+","+this.g+","+this.b+")")
+        this.eu.setAttribute("style",`left:${this.px}px;top:${this.py}px;width:${this.tam}px;height:${this.tam}px;background-color:rgb(${this.r},${this.g},${this.b})`)
         if((this.px > larguraPalco)||(this.py > alturaPalco)){
             this.remover()
         }
