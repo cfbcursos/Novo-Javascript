cdf07227abc8b3c3a1bf2021cf8a3a1f9fa448f3
CFB Cursos <canalfessorbruno@gmail.com>
2023-03-11 12:01:13 -0300

    atualizações 149

---
+++ aula150.js
@@ -0,0 +1,17 @@
+import { Cxmsg } from "./cxmsg.js";
+
+const callback_ok=()=>{
+
+}
+
+const callback_naook=()=>{
+    const config={
+        cor:"#800",
+        tipo:"ok",
+        textos:null,
+        comando_sn:null,
+    }
+    Cxmsg.mostrar(config,"Erro","Login não efetuado! Usuário ou senha incorretos.");
+}
+
+// Login.login(callback_ok,callback_naook);
