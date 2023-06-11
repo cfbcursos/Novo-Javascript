36726589845379687213d5f8eedd543933b49880
CFB Cursos <canalfessorbruno@gmail.com>
2023-06-11 19:18:23 -0300

    js

--- Estoque/backoffice/colaboradores/colaboradores.js
+++ Estoque/backoffice/colaboradores/colaboradores.js
@@ -2,6 +2,7 @@ const dadosGrid=document.querySelector("#dadosGrid");
 const btn_add=document.querySelector("#btn_add");
 const novoColaborador=document.querySelector("#novoColaborador");
 const btn_fecharPopup=document.querySelector("#btn_fecharPopup");
+const btn_fecharPopupPesq=document.querySelector("#btn_fecharPopupPesq");
 const btn_gravarPopup=document.querySelector("#btn_gravarPopup");
 const btn_cancelarPopup=document.querySelector("#btn_cancelarPopup");
 const telefones=document.querySelector("#telefones");
@@ -11,61 +12,227 @@ const f_tipoColab=document.querySelector("#f_tipoColab");
 const f_status=document.querySelector("#f_status");
 const f_foto=document.querySelector("#f_foto");
 const img_foto=document.querySelector("#img_foto");
+const f_filtragem=document.querySelector("#f_filtragem");
+const pesquisa=document.querySelector("#pesquisa");
+const btn_pesq=document.querySelector("#btn_pesq");
+const f_pesqId=document.querySelector("#f_pesqId");
+const f_pesqNome=document.querySelector("#f_pesqNome");
+const btn_pesquisar=document.querySelector("#btn_pesquisar");
+const btn_listarTudo=document.querySelector("#btn_listarTudo");
 
-const endpoint_todoscolaboradores=`http://127.0.0.1:1880/todosusuarios`;
-fetch(endpoint_todoscolaboradores)
-.then(res=>res.json())
-.then(res=>{
-    console.log(res);
-    dadosGrid.innerHTML="";
-    res.forEach(e=>{
-        const divlinha=document.createElement("div");
-        divlinha.setAttribute("class","linhaGrid");
+//n=Novo Colaborador | e=Editar colaborador
+let modojanela="n";
+const serv=sessionStorage.getItem("servidor_nodered");
 
-        const divc1=document.createElement("div");
-        divc1.setAttribute("class","colunaLinhaGrid c1");
-        divc1.innerHTML=e.n_usuario_usuario;
-        divlinha.appendChild(divc1);
-
-        const divc2=document.createElement("div");
-        divc2.setAttribute("class","colunaLinhaGrid c2");
-        divc2.innerHTML=e.s_nome_usuario;
-        divlinha.appendChild(divc2);
-        
-        const divc3=document.createElement("div");
-        divc3.setAttribute("class","colunaLinhaGrid c3");
-        divc3.innerHTML=e.n_tipousuario_tipousuario;
-        divlinha.appendChild(divc3);
-        
-        const divc4=document.createElement("div");
-        divc4.setAttribute("class","colunaLinhaGrid c4");
-        divc4.innerHTML=e.c_status_usuario;
-        divlinha.appendChild(divc4);    
-        
-        const divc5=document.createElement("div");
-        divc5.setAttribute("class","colunaLinhaGrid c5");
-        divlinha.appendChild(divc5);
-        
-        const img_status=document.createElement("img");
-        img_status.setAttribute("src","../../imgs/on.svg");
-        img_status.setAttribute("class","icone_op");
-        divc5.appendChild(img_status);
-
-        const img_editar=document.createElement("img");
-        img_editar.setAttribute("src","../../imgs/editar.svg");
-        img_editar.setAttribute("class","icone_op");
-        divc5.appendChild(img_editar);
-        
-        const img_remover=document.createElement("img");
-        img_remover.setAttribute("src","../../imgs/delete.svg");
-        img_remover.setAttribute("class","icone_op");
-        divc5.appendChild(img_remover); 
-
-        dadosGrid.appendChild(divlinha);
-    });
+f_filtragem.addEventListener("keyup",(evt)=>{
+    const linhas=[...document.querySelectorAll(".linhaGrid")];
+    let input,texto,filtragem;
+    input=evt.target;
+    filtragem=input.value.toUpperCase();
+    for(let i=0;i<linhas.length;i++){
+        texto=linhas[i].children[1].innerHTML;
+        if(texto.toUpperCase().indexOf(filtragem)>-1){
+            linhas[i].classList.remove("ocultarLinhaGrid");
+        }else{
+            linhas[i].classList.add("ocultarLinhaGrid");
+        }
+    }
+});
+btn_fecharPopupPesq.addEventListener("click",(evt)=>{
+    pesquisa.classList.add("ocultarPopup");
+});
+btn_pesq.addEventListener("click",(evt)=>{
+    pesquisa.classList.remove("ocultarPopup");
+    f_pesq.value="";
+    f_pesq.focus();
+});
+f_pesqId.addEventListener("click",(evt)=>{
+    f_pesq.value="";
+    f_pesq.focus();
+});
+f_pesqNome.addEventListener("click",(evt)=>{
+    f_pesq.value="";
+    f_pesq.focus();
+});
+btn_pesquisar.addEventListener("click",(evt)=>{
+    let tipo=null;
+    if(f_pesqId.checked){
+        tipo="id";
+    }else{
+        tipo="nome";
+    }
+    if(f_pesq.value!=""){
+        const endpointpesq=`${serv}/pesquisacolab/${tipo}/${f_pesq.value}`;
+        fetch(endpointpesq)
+        .then(res=>res.json())
+        .then(res=>{
+            dadosGrid.innerHTML="";
+            res.forEach(e=>{
+                criarLinha(e);
+            });
+        })
+        pesquisa.classList.add("ocultarPopup");
+    }else{
+        alert("Preecha o campo de pesquisa");
+        f_pesq.focus();
+    }
 });
 
-const endpoint_tiposColab=`http://127.0.0.1:1880/tiposcolab`;
+btn_listarTudo.addEventListener("click",(evt)=>{
+    carregarTodosColabs();
+});
+
+const criarCxTelefone=(fone,idtel,tipo)=>{
+    const divTel=document.createElement("div");
+    divTel.setAttribute("class","tel");
+
+    const numTel=document.createElement("div");
+    if(tipo=="n"){
+        numTel.setAttribute("class","numTel novoTel");
+    }else{
+        numTel.setAttribute("class","numTel editarTel");
+    }
+
+    numTel.innerHTML=fone;
+    divTel.appendChild(numTel);
+
+    const delTel=document.createElement("img");
+    delTel.setAttribute("src","../../imgs/delete.svg");
+    delTel.setAttribute("class","delTel");
+    delTel.setAttribute("data-idtel",idtel);
+    delTel.addEventListener("click",(evt)=>{
+        if(idtel!="-1"){
+            const objTel=evt.target;
+            const idtel=objTel.dataset.idtel;
+            const endpoint_delTelefone=`${serv}/deltelefone/${idtel}`;
+            fetch(endpoint_delTelefone)
+            .then(res=>{
+                if(res.status==200){
+                    evt.target.parentNode.remove();
+                }
+            })
+        }else{
+            evt.target.parentNode.remove();
+        }
+    });
+    divTel.appendChild(delTel);
+    
+    telefones.appendChild(divTel);
+}
+
+const carregarTodosColabs=()=>{
+    const endpoint_todoscolaboradores=`${serv}/todosusuarios`;
+    fetch(endpoint_todoscolaboradores)
+    .then(res=>res.json())
+    .then(res=>{
+        dadosGrid.innerHTML="";
+        res.forEach(e=>{
+            criarLinha(e);
+        });
+    });
+}
+carregarTodosColabs();
+
+const criarLinha=(e)=>{
+    const divlinha=document.createElement("div");
+    divlinha.setAttribute("class","linhaGrid");
+
+    const divc1=document.createElement("div");
+    divc1.setAttribute("class","colunaLinhaGrid c1");
+    divc1.innerHTML=e.n_usuario_usuario;
+    divlinha.appendChild(divc1);
+
+    const divc2=document.createElement("div");
+    divc2.setAttribute("class","colunaLinhaGrid c2");
+    divc2.innerHTML=e.s_nome_usuario;
+    divlinha.appendChild(divc2);
+    
+    const divc3=document.createElement("div");
+    divc3.setAttribute("class","colunaLinhaGrid c3");
+    divc3.innerHTML=e.n_tipousuario_tipousuario;
+    divlinha.appendChild(divc3);
+    
+    const divc4=document.createElement("div");
+    divc4.setAttribute("class","colunaLinhaGrid c4");
+    divc4.innerHTML=e.c_status_usuario;
+    divlinha.appendChild(divc4);    
+    
+    const divc5=document.createElement("div");
+    divc5.setAttribute("class","colunaLinhaGrid c5");
+    divlinha.appendChild(divc5);
+    
+    const img_status=document.createElement("img");
+    if(e.c_status_usuario=="A"){
+        img_status.setAttribute("src","../../imgs/on.svg");
+    }else{
+        img_status.setAttribute("src","../../imgs/off.svg");
+    }
+    img_status.setAttribute("data-idcolab",e.n_usuario_usuario);
+    img_status.setAttribute("class","icone_op");
+    img_status.addEventListener("click",(evt)=>{
+        const idcolab=evt.target.dataset.idcolab;
+        if(evt.target.getAttribute("src")=="../../imgs/on.svg"){
+            const endpoint_mudarStatus=`${serv}/mudarStatusColab/${idcolab}/I`;
+            fetch(endpoint_mudarStatus)
+            .then(res=>{
+                if(res.status==200){
+                    evt.target.setAttribute("src","../../imgs/off.svg");
+                    evt.target.parentNode.parentNode.childNodes[3].innerHTML="I";
+                }
+            })
+        }else{
+            const endpoint_mudarStatus=`${serv}/mudarStatusColab/${idcolab}/A`;
+            fetch(endpoint_mudarStatus)
+            .then(res=>{
+                if(res.status==200){
+                    evt.target.setAttribute("src","../../imgs/on.svg");
+                    evt.target.parentNode.parentNode.childNodes[3].innerHTML="A";
+                }
+            })
+        }
+    })
+    divc5.appendChild(img_status);
+
+    const img_editar=document.createElement("img");
+    img_editar.setAttribute("src","../../imgs/editar.svg");
+    img_editar.setAttribute("class","icone_op");
+    img_editar.addEventListener("click",(evt)=>{
+        const id=evt.target.parentNode.parentNode.firstChild.innerHTML;
+        modojanela="e";
+        document.getElementById("tituloPopup").innerHTML="Editar Colaborador";
+        let endpoint=`${serv}/dadoscolab/${id}`;
+        fetch(endpoint)
+        .then(res=>res.json())
+        .then(res=>{
+            btn_gravarPopup.setAttribute("data-idcolab",id);
+            f_nome.value=res[0].s_nome_usuario;
+            f_tipoColab.value=res[0].n_tipousuario_tipousuario;
+            f_status.value=res[0].c_status_usuario;
+            img_foto.src=res[0].s_foto_usuario;
+            novoColaborador.classList.remove("ocultarPopup");
+        })
+
+        endpoint=`${serv}/telefonescolab/${id}`;
+        fetch(endpoint)
+        .then(res=>res.json())
+        .then(res=>{
+            telefones.innerHTML="";
+            res.forEach(t=>{
+                criarCxTelefone(t.s_numero_telefone,t.n_telefone_telefone,"e");
+            })
+        })
+    });
+    divc5.appendChild(img_editar);
+    
+    const img_remover=document.createElement("img");
+    img_remover.setAttribute("src","../../imgs/delete.svg");
+    img_remover.setAttribute("class","icone_op");
+    divc5.appendChild(img_remover); 
+
+    dadosGrid.appendChild(divlinha);
+}
+
+const endpoint_tiposColab=`${serv}/tiposcolab`;
 fetch(endpoint_tiposColab)
 .then(res=>res.json())
 .then(res=>{
@@ -79,19 +246,28 @@ fetch(endpoint_tiposColab)
 })
 
 btn_add.addEventListener("click",(evt)=>{
+    modojanela="n";
+    document.getElementById("tituloPopup").innerHTML="Novo Colaborador";
     novoColaborador.classList.remove("ocultarPopup");
+    f_nome.value="";
+    f_tipoColab.value="";
+    f_status.value="";
+    f_foto.value="";
+    img_foto.setAttribute("src","#");
+    telefones.innerHTML="";
 });
 btn_fecharPopup.addEventListener("click",(evt)=>{
     novoColaborador.classList.add("ocultarPopup");
 });
 btn_gravarPopup.addEventListener("click",(evt)=>{
-    const tels=[...document.querySelectorAll(".numTel")];
+    const tels=[...document.querySelectorAll(".novoTel")];
     console.log(tels);
     let numTels=[];
     tels.forEach(t=>{
         numTels.push(t.innerHTML);
     });
     const dados={
+        n_usuario_usuario:evt.target.dataset.idcolab,
         s_nome_usuario:f_nome.value,
         n_tipousuario_tipousuario:f_tipoColab.value,
         c_status_usuario:f_status.value,
@@ -102,22 +278,31 @@ btn_gravarPopup.addEventListener("click",(evt)=>{
         method:'post',
         body:JSON.stringify(dados)
     }
-    const endpointnovocolab=`http://127.0.0.1:1880/novocolab`
-    fetch(endpointnovocolab,cab)
+    let endpointnovoeditarcolab=null;
+    if(modojanela=="n"){
+        endpointnovoeditarcolab=`${serv}/novocolab`
+    }else{
+        endpointnovoeditarcolab=`${serv}/editarcolab`
+    }
+    fetch(endpointnovoeditarcolab,cab)
     .then(res=>{
         if(res.status==200){
-            alert("Novo colaborador gravado");
-            f_nome.value="";
-            f_tipoColab.value="";
-            f_status.value="";
-            f_foto.value="";
-            img_foto.setAttribute("src","#");
-            telefones.innerHTML="";
+            if(modojanela=="n"){
+                alert("Novo colaborador gravado");
+                f_nome.value="";
+                f_tipoColab.value="";
+                f_status.value="";
+                f_foto.value="";
+                img_foto.setAttribute("src","#");
+                telefones.innerHTML="";
+                carregarTodosColabs();
+            }else{
+                alert("Colaborador editaco com sucesso");
+            }
         }else{
             alert("Erro ao gravar novo colaborador");
         }
     })
-    
 });
 btn_cancelarPopup.addEventListener("click",(evt)=>{
     novoColaborador.classList.add("ocultarPopup");
@@ -126,24 +311,7 @@ btn_cancelarPopup.addEventListener("click",(evt)=>{
 f_fone.addEventListener("keyup",(evt)=>{
     if(evt.key=="Enter"){
         if(evt.target.value.length >= 8){
-            const divTel=document.createElement("div");
-            divTel.setAttribute("class","tel");
-
-            const numTel=document.createElement("div");
-            numTel.setAttribute("class","numTel");
-            numTel.innerHTML=evt.target.value;
-            divTel.appendChild(numTel);
-
-            const delTel=document.createElement("img");
-            delTel.setAttribute("src","../../imgs/delete.svg");
-            delTel.setAttribute("class","delTel");
-            delTel.addEventListener("click",(evt)=>{
-                evt.target.parentNode.remove();
-            });
-            divTel.appendChild(delTel);
-            
-            telefones.appendChild(divTel);
-
+            criarCxTelefone(evt.target.value,"-1","n");
             evt.target.value="";
         }else{
             alert("Número de Telefone inválido");
--- Estoque/backoffice/index.js
+++ Estoque/backoffice/index.js
@@ -2,6 +2,15 @@ const btn_menuPrincipal=document.querySelector("#btn_menuPrincipal");
 const menuPrincipal=document.querySelector("#menuPrincipal");
 const todosmenusprincipais=[...document.querySelectorAll(".btn_menuItem")];
 
+const endpoint_config=`../config.txt`;
+fetch(endpoint_config)
+.then(res=>res.json())
+.then(res=>{
+    sessionStorage.setItem("servidor_nodered",res.servidor_nodered);
+    sessionStorage.setItem("versao",res.versao);
+})
+
+
 btn_menuPrincipal.addEventListener("click",(evt)=>{
     menuPrincipal.classList.toggle("ocultar");
 });
