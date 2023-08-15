267b69ad77e55cf3551c11c4454e65b6d7afd7d8
CFB Cursos <canalfessorbruno@gmail.com>
2023-08-14 23:41:29 -0300

    js

--- Estoque/backoffice/index.js
+++ Estoque/backoffice/index.js
@@ -5,6 +5,15 @@ const f_senha=document.querySelector("#f_senha");
 const btn_login=document.querySelector("#btn_login");
 const primeiroAcessso=document.querySelector("#primeiroAcessso");
 const login=document.querySelector("#login");
+const emaildefsenha=document.querySelector("#emaildefsenha");
+const btn_fecharPopupDefSenha=document.querySelector("#btn_fecharPopupDefSenha");
+const iddefsenha=document.querySelector("#iddefsenha");
+const f_senha1=document.querySelector("#f_senha1");
+const f_senha2=document.querySelector("#f_senha2");
+const btn_gravarSenha=document.querySelector("#btn_gravarSenha");
+
+sessionStorage.setItem("n_pessoa_pessoa","-1");
+sessionStorage.setItem("s_nome_pessoa","-1");
 
 let serv=null;
 
@@ -26,10 +35,13 @@ btn_login.addEventListener("click",(evt)=>{
         }
         const endpoint=`${serv}/login/${email}/${senha}`;
         fetch(endpoint)
+        .then(res=>res.json())
         .then(res=>{
-            if(res.status==200){ //OK
+            if(res[0].retorno==200){ //OK
+                sessionStorage.setItem("n_pessoa_pessoa",res[0].n_pessoa_pessoa);
+                sessionStorage.setItem("s_nome_pessoa",res[0].s_nome_pessoa);
                 window.location.href="./main.html";
-            }else if(res.status==208){ //Senha errada
+            }else if(res[0].retorno==208){ //Senha errada
                 const config={
                     titulo:"Erro",
                     texto:"Senha Incorreta",
@@ -40,11 +52,64 @@ btn_login.addEventListener("click",(evt)=>{
                     nao:()=>{}
                 }
                 Cxmsg.mostrar(config);
-            }else if(res.status==205){ //Primeiro Acesso
+            }else if(res[0].retorno==205){ //Primeiro Acesso
                 console.log("Primeiro Acesso");
+                iddefsenha.value=res[0].n_pessoa_pessoa;
+                emaildefsenha.value=f_email.value;
                 login.classList.add("ocultarPopup");
                 primeiroAcessso.classList.remove("ocultarPopup");
             }
         })
+    }
+});
+
+btn_fecharPopupDefSenha.addEventListener("click",(evt)=>{
+    primeiroAcessso.classList.add("ocultarPopup");
+    login.classList.remove("ocultarPopup");
+});
+
+btn_gravarSenha.addEventListener("click",(evt)=>{
+    if(f_senha1.value!="" && f_senha2.value!=""){
+        if(f_senha1.value != f_senha2.value){
+            const config={
+                titulo:"Alerta",
+                texto:"Senhas não são iguais",
+                cor:"#008",
+                tipo:"ok",
+                ok:()=>{},
+                sim:()=>{},
+                nao:()=>{}
+            }
+            Cxmsg.mostrar(config);
+        }else{
+            const dados={
+                n_pessoa_pessoa:iddefsenha.value,
+                s_senha_pessoa:f_senha1.value,
+            }
+            const cab={
+                method:'post',
+                body:JSON.stringify(dados)
+            }
+            let endpoint=`${serv}/gravarnovasenha`;
+            fetch(endpoint,cab)
+            .then(res=>{
+                if(res.status==200){
+                    primeiroAcessso.classList.add("ocultarPopup");
+                    login.classList.remove("ocultarPopup");
+                    f_senha.value=f_senha1.value;
+                }
+            })
+        }
+    }else{
+        const config={
+            titulo:"Alerta",
+            texto:"Digite a senha nos dois campos",
+            cor:"#008",
+            tipo:"ok",
+            ok:()=>{},
+            sim:()=>{},
+            nao:()=>{}
+        }
+        Cxmsg.mostrar(config);
     }
 });
--- Estoque/backoffice/main.js
+++ Estoque/backoffice/main.js
@@ -1,6 +1,13 @@
+if(sessionStorage.getItem("n_pessoa_pessoa")=="-1"){
+    window.location.href="./index.html";
+}
+
 const btn_menuPrincipal=document.querySelector("#btn_menuPrincipal");
 const menuPrincipal=document.querySelector("#menuPrincipal");
 const todosmenusprincipais=[...document.querySelectorAll(".btn_menuItem")];
+const divid=document.querySelector("#id");
+const divnome=document.querySelector("#nome");
+const btnlogoff=document.querySelector("#btnlogoff");
 
 btn_menuPrincipal.addEventListener("click",(evt)=>{
     menuPrincipal.classList.toggle("ocultar");
@@ -11,6 +18,19 @@ todosmenusprincipais.forEach(e=>{
         menuPrincipal.classList.add("ocultar");
     });
 })
+
+btnlogoff.addEventListener("click",(evt)=>{
+    sessionStorage.setItem("n_pessoa_pessoa","-1");
+    sessionStorage.setItem("s_nome_pessoa","-1");
+    window.location.href="./index.html";
+});
+
+const n_pessoa_pessoa=sessionStorage.getItem("n_pessoa_pessoa");
+const s_nome_pessoa=sessionStorage.getItem("s_nome_pessoa");
+divid.innerHTML=`id:${n_pessoa_pessoa}`;
+divnome.innerHTML=`nome:${s_nome_pessoa}`;
+
+
 
 
 
